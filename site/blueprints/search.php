b371dadfd5ee3e36e49df7f964f333900aa58650
Kim <kim@franken.mx>
2015-09-15 16:13:08 +0200

    add search

---
+++ site/blueprints/search.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Search
+pages: false
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
